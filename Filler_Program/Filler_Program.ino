724ed259b1e07f3e57f0516d6bb58acf6006a610
senoird <senoird@gmail.com>
2018-12-30 10:47:58 -0600

    Add files via upload

--- Filler_Program/Filler_Program.ino
+++ Filler_Program/Filler_Program.ino
@@ -47,7 +47,7 @@ void loop() {
 void raise_() {
   digitalWrite(dirPin,HIGH); // Enables the motor to move in a particular direction
   // Makes 200 pulses for making one full cycle rotation
-  for(int x = 0; x < 800; x++) {
+  for(int x = 0; x < 3810; x++) {
     digitalWrite(stepPin,HIGH); 
     delayMicroseconds(500); 
     digitalWrite(stepPin,LOW); 
@@ -57,7 +57,7 @@ void raise_() {
 void lower() {
   digitalWrite(dirPin,LOW); //Changes the rotations direction
   // Makes 400 pulses for making two full cycle rotation
-  for(int x = 0; x < 800; x++) {
+  for(int x = 0; x < 3810; x++) {
     digitalWrite(stepPin,HIGH);
     delayMicroseconds(500);
     digitalWrite(stepPin,LOW);
